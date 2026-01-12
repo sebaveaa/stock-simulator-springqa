47eb8144b3000d1a322e5dbf0da27cdcd4523e4c
sebaveaa <sebaveaa@gmail.com>
2026-01-12 13:39:35 -0400

    tests arreglados, probandoo

--- src/test/java/ucab/edu/ve/stocksimulator/StockSimulatorApplicationTests.java
+++ src/test/java/ucab/edu/ve/stocksimulator/StockSimulatorApplicationTests.java
@@ -2,8 +2,10 @@ package ucab.edu.ve.stocksimulator;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class StockSimulatorApplicationTests {
 
 	@Test
